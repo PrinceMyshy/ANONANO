c0d2376e8528f22f8a8fa736c8333510ed3de824
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-04-25 12:37:04 -0400

    main2

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from http import client
+from discord import Client
 import nextcord
 from nextcord import interactions
 from nextcord.ext import commands
 
 intents = nextcord.Intents.default()
 intents.members = True
+
+client = commands.BOT(command_prefix = '!', intents = intents) 
+
+@client.event
+async def on_ready():
+    print("The bot is now ready for use")
+    
+
+client.run('')
