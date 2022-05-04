e763023b1131fb879a34169902b379bde5f67a15
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-05-03 21:26:41 -0400

    logging

--- master_.py
+++ master_.py
@@ -37,11 +37,12 @@ async def on_message(message):
                 
 client.run(token)
 
-#logs
-'''
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-'''
