cfc48e3474986b08b99941c206a156d0096a7d00
PrinceDZ <68832217+PrinceMyshy@users.noreply.github.com>
2022-05-21 19:10:18 -0400

    format fixed

--- master_.py
+++ master_.py
@@ -1,73 +1,68 @@
+import types
+import discord
 from cgi import test
 import os
-import discord
 import logging
 import random
-
-import asyncio
-import collections
-import inspect
-import importlib.util
-import sys
-import traceback
-import types
-
-import discord
-
-from .core import GroupMixin
-from .view import StringView
-from .context import Context
-from . import errors
-from .help import HelpCommand, DefaultHelpCommand
-from .cog import Cog
-
-
 from discord.ext import commands
-
 from dotenv import load_dotenv
-load_dotenv()
+from discord import Embed
 
+load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+######################################################################################################################################
 
-#import logging 
+####################################################################################################
+                                      # Replying to Messages #
+####################################################################################################
 
 #logging on
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
 
+client = discord.Client(intents=intents)
 
 @client.event
-async def on_ready():
-    print('I am logged in now as {0.user}'.format(client))
-
-#responging to messages
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_ready(self):
+    print(f'I am now logged in as {self.user} (ID: {self.user.id})')
+    print('------')
+        
+async def on_message(self, message):
+    if message.author.id == self.user.id:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#############################################
+                # Messages #
+#############################################
+
+    if message.content.startswith('!hello'):
+        await message.reply('Hello!', mention_author=True)
 
     if message.content.startswith('halal'):
         await message.channel.send("is pork halal?")
-        
+            
     if "haram" in message.content:
         await message.channel.send("abstain from haram!")
-        
+            
     if 'banned word' in message.content:
         await message.channel.send("That word is banned!")
-    
+        
     if 'what kinda question is that' in message.content:
         await message.channel.send("I honestly don't know! What an idiot.")
         
+             
+####################################################################################################
+                                            # COMMANDS #
+####################################################################################################
+   
 
 client = commands.Bot(command_prefix=".")        
 
 @client.command()
-async def question(ctx, ask: ctx.message):
+async def question(ctx, arg):
     random = ["love", "stop", "no", "hmm... gay", "why?", "is math related to science?"]
-    await ctx.send(f"**You asked:** *{ask}* | **and I say**: *{random.choice(random)}*")
+    await ctx.send(f"**You asked:** *{arg}* | **and I say**: *{random.choice(random)}*")
 
 @client.command()
 async def ship(ctx, member_1: discord.Member, member_2: discord.Member):
@@ -76,7 +71,8 @@ async def ship(ctx, member_1: discord.Member, member_2: discord.Member):
     rate = randint(1, 100)
     await ctx.send(f"""**{member_1}** | :heart: {rate}% :heart: | **{member_2}**""")
 
-                
+         
+
 client.run(token)
 
 '''
